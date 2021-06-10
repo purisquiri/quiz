e8503f24bf952d68b6cd4d2a9aad696570e382f1
purisquiri <mau.dorigo@outlook.es>
2021-06-10 15:18:52 -0400

    transitions added

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -9,22 +9,24 @@
         {{ questionsAnswered }} out of {{ questions.length }} questions answered
       </div>
     </div>
-    <div
-      class="single-question"
-      v-for="(question, qi) in questions"
-      :key="question.q"
-      v-show="questionsAnswered === qi"
-    >
-      <div class="question">{{ question.q }}</div>
+    <transition-group name="fade">
       <div
-        class="answers"
-        v-for="answer in question.answers"
-        :key="answer.text"
-        @click.prevent="selectAnswer(answer.is_correct)"
+        class="single-question"
+        v-for="(question, qi) in questions"
+        :key="question.q"
+        v-show="questionsAnswered === qi"
       >
-        <div class="answer">{{ answer.text }}</div>
+        <div class="question">{{ question.q }}</div>
+        <div
+          class="answers"
+          v-for="answer in question.answers"
+          :key="answer.text"
+          @click.prevent="selectAnswer(answer.is_correct)"
+        >
+          <div class="answer">{{ answer.text }}</div>
+        </div>
       </div>
-    </div>
+    </transition-group>
   </div>
 </template>
 
