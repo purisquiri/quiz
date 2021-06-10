307fccd4fe60335b0ccaaec417b571acb9386d69
purisquiri <mau.dorigo@outlook.es>
2021-06-10 11:14:53 -0400

    questions and answers changing after clicking

---
+++ src/components/Questions.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="questions-ctr">
+    <div class="progress">
+      <div class="bar"></div>
+      <div class="status">1 out of 3 questions answered</div>
+    </div>
+    <div
+      class="single-question"
+      v-for="(question, qi) in questions"
+      :key="question.q"
+      v-show="questionsAnswered === qi"
+    >
+      <div class="question">{{ question.q }}</div>
+      <div
+        class="answers"
+        v-for="answer in question.answers"
+        :key="answer.text"
+        @click.prevent="selectAnswer(answer.is_correct)"
+      >
+        <div class="answer">{{ answer.text }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["questions", "questionsAnswered"],
+  emit: ["question-answered"],
+  methods: {
+    selectAnswer(is_correct) {
+      this.$emit("question-answered", is_correct);
+    },
+  },
+};
+</script>
